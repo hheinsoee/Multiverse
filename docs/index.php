33580861c07e926fc944bce850f89759381fb0c4
Hein Soe <35089574+hheinsoee@users.noreply.github.com>
2021-09-30 22:34:57 +0630

    Ready

--- docs/index.php
+++ docs/index.php
@@ -11,14 +11,50 @@ function theInfo(){
     <?php include_once "head.php";?>    
 </head>
 <body>
-    <center>
-        <h1><?=theInfo()->name;?></h1>
-        <p><?=theInfo()->description;?></p>
-        <img src="<?=theInfo()->logo;?>" alt="" id="logo">
-        <div>
-            <a href="<?=theInfo()->fb_page_id;?>" target="_blank">facebook</a>
-            <a href="<?=theInfo()->app_download_link;?>" target="_blank">download</a>
+    <video autoplay muted loop download="false" id="myVideo"
+            style="
+            position: fixed;
+            right: 0;
+            bottom: 0;
+            min-width: 100vw;
+            min-height: 100vh;
+            ">
+            <source src="media/bg.webm" type="video/mp4">
+    </video>
+    <div style="
+        display: flex;
+        align-items: center;
+		justify-content:center;
+        min-height: 100vh;
+        background-color: rgb(0 14 106 / 35%);
+        position: relative;
+        backdrop-filter:blur(3px);
+        ">
+		<div style="
+                display:flex;
+				flex:1;
+				color:#fff;
+                max-width:400px;
+                margin:10px;
+				">
+            <center>
+                <img src="<?=theInfo()->logo;?>" alt="" id="logo">
+                <h1><?=theInfo()->name;?></h1>
+                <div class="flex _ce">
+                    <img src="media/Flag_of_Myanmar.svg" alt="" id="ico">
+                    <div>&nbsp;မြန်မာစာတန်းထိုး အကြည် ဇတ်ကားများ</div>
+                </div>
+                <p id="description"><?=theInfo()->description;?></p>
+                <div class="flex _ce">
+                    <a id="icoBtn" href="https://fb.com/<?=theInfo()->fb_page_id;?>" target="_blank">
+                        <img src="media/Facebook_icon_2013.svg" alt="" id="ico">
+                    </a>
+                    <a id="icoBtn" href="<?=theInfo()->app_download_link;?>" target="_blank">
+                        <img src="media/android.svg" alt="" id="ico">&nbsp;download
+                    </a>
+                </div>
+            </center>
         </div>
-    </center>
+    </div>
 </body>
 </html>
